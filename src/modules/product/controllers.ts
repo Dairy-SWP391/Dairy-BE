d9ea1dd5543a9ec6ea9c391fccd87ee5b878b3e5
phm.giamy@gmail.com <phm.giamy@gmail.com>
2024-06-01 00:28:36 +0700

    feat: add product api

--- src/modules/product/controllers.ts
+++ src/modules/product/controllers.ts
@@ -2,6 +2,8 @@ import { Request, Response } from 'express';
 import productService from './service';
 import { PRODUCT_MESSAGES } from './messages';
 import productPricingsService from '../product_pricings/service';
+import { ParamsDictionary } from 'express-serve-static-core';
+import { AddProductBodyReq } from './requests';
 
 export const getProductDetailController = async (req: Request, res: Response) => {
   const { id } = req.params;
@@ -14,5 +16,15 @@ export const getProductDetailController = async (req: Request, res: Response) =>
   res.status(200).json({
     data: { ...product, ...prices },
     message: PRODUCT_MESSAGES.GET_PRODUCT_DETAIL_SUCCESS,
+  });
+};
+export const addProductController = async (
+  req: Request<ParamsDictionary, any, AddProductBodyReq>,
+  res: Response,
+) => {
+  const result = await productService.addProduct(req.body);
+  res.json({
+    message: PRODUCT_MESSAGES.ADD_PRODUCT_SUCCESS,
+    result: result,
   });
 };
--- src/modules/product/routes.ts
+++ src/modules/product/routes.ts
@@ -1,9 +1,15 @@
 import { Router } from 'express';
 import { wrapAsync } from '~/utils/handler';
-import { getProductDetailController } from './controllers';
+import { addProductController, getProductDetailController } from './controllers';
+import { roleValidator, addProductValidator } from './middlewares';
 
 const productRouter = Router();
-
+productRouter.get(
+  '/add-product',
+  roleValidator,
+  addProductValidator,
+  wrapAsync(addProductController),
+);
 productRouter.get('/:id', wrapAsync(getProductDetailController));
 
 export default productRouter;
