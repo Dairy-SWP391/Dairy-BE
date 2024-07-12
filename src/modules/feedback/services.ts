9238785e1c44fef2df77feb0c6feab86dbe12a90
Forpgm <phm.giamy@gmail.com>
2024-07-12 09:05:01 +0700

    fix: return feedback for product detail api

--- src/modules/feedback/services.ts
+++ src/modules/feedback/services.ts
@@ -9,6 +9,33 @@ class FeedbackService {
 
     return result;
   }
+
+  async getFeedbackByProductId(product_id: number) {
+    const result = await DatabaseInstance.getPrismaInstance().orderDetail.findMany({
+      where: {
+        product_id,
+      },
+      select: {
+        id: true,
+      },
+    });
+
+    const rs = await DatabaseInstance.getPrismaInstance().feedback.findMany({
+      where: {
+        order_detail_id: {
+          in: result.map((item) => item.id),
+        },
+      },
+      select: {
+        user_id: true,
+        content: true,
+        rating_point: true,
+        created_at: true,
+      },
+    });
+
+    return rs;
+  }
 }
 const feedbackService = new FeedbackService();
 export default feedbackService;
